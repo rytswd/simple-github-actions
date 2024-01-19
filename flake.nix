e87f0d9a0a8357d2cfab4bc1d360d1aa2d73ffa5
Ryota <rytswd@gmail.com>
2024-01-19 14:06:42 +0000

    Add Nix Flake and direnv setup

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Testing with Simple GitHub Actions";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs-unstable.legacyPackages.${system};
+
+        basePackages = with pkgs; [
+          act
+          gh
+        ];
+
+        customOverrides = self: super: {
+          # Overrides go here
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = basePackages;
+        };
+      }
+    );
+}
